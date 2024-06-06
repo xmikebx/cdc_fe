704cb2de46c0b4e4cfb319d89dd5dd23248c7419
xmikebx <xmikebx@gmail.com>
2024-06-06 17:50:38 +0100

    chore: FE02 navbar setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,14 @@
+import { useState, useEffect } from "react";
+import Navbar from "./components/navbar/Navbar";
+import "./App.css";
 
+function App() {
+  const [user, setUser] = useState({});
+  return (
+    <div className="wrapper">
+      <Navbar />
+    </div>
+  );
+}
+
+export default App;
